2eb7945a05ffc67f92ffa0670c03dc6a7fffe454
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-05 10:31:00 -0300

    Calculo de dia, ano e mês com Calendar Módulo 15 aula 15.5 :beetle:

--- src/cursojava/main/ObjetoCalendar.java
+++ src/cursojava/main/ObjetoCalendar.java
@@ -1,5 +1,6 @@
 package cursojava.main;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -8,7 +9,7 @@ import cursojava.excecao.Excecao;
 
 public class ObjetoCalendar {
 
-	public static void main(String[] args) throws Excecao {
+	public static void main(String[] args) {
 		
 //		Calendar calendar = Calendar.getInstance(); 
 //		System.out.println("Calendar em milisegundos :: "+calendar.getTimeInMillis());
@@ -37,31 +38,54 @@ public class ObjetoCalendar {
 //		System.out.println("Calendar data atual em formato padrão e string :: " 
 //		+ sdf.format(calendar.getInstance().getTime()));
 		
+//		try {
+//			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+//			Date dataVencimentoBoleto = sdf.parse("09-07-2022");//data 1
+//			Date dataAtualHoje = sdf.parse("10-07-2022");// data 2
+//			
+//			//comparando datas
+//			//AFTER É POSTERIOR, MAIOR OU DEPOIS DATA ATUAL
+//			/*Se a data de vencimento é maior que a data atual*/
+//			if (dataVencimentoBoleto.after(dataAtualHoje)) {
+//				System.out.println("Boleto não vencido!");
+//			} else {
+//				System.out.println("Boleto vencido!");
+//			}
+//			
+//			//comparando datas
+//			//BEFORE É ANTES, MENOR OU IGUAL A DATA ATUAL
+//			/*Se a data de vencimento é menor que a data atual*/
+//			if (dataVencimentoBoleto.before(dataAtualHoje)) {
+//				System.out.println("Boleto vencido - URGENTE!");
+//			} else {
+//				System.out.println("Boleto não vencido!");
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			throw new Excecao(e.getMessage());
+//		}
+		
+		
+		Calendar calendar = Calendar.getInstance();//PEGA A DATA ATUAL
+		
+		/*SIMULAR QUE DATA VEM DO BANCO DE DADOS*/
 		try {
-			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-			Date dataVencimentoBoleto = sdf.parse("09-07-2022");//data 1
-			Date dataAtualHoje = sdf.parse("10-07-2022");// data 2
+			calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("08-07-2022"));
+			calendar.add(Calendar.DAY_OF_MONTH, 30);/*Data de hoje, e acrescenta 30 dias*/
 			
-			//comparando datas
-			//AFTER É POSTERIOR, MAIOR OU DEPOIS DATA ATUAL
-			/*Se a data de vencimento é maior que a data atual*/
-			if (dataVencimentoBoleto.after(dataAtualHoje)) {
-				System.out.println("Boleto não vencido!");
-			} else {
-				System.out.println("Boleto vencido!");
-			}
+			System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+			System.out.println();
 			
-			//comparando datas
-			//BEFORE É ANTES, MENOR OU IGUAL A DATA ATUAL
-			/*Se a data de vencimento é menor que a data atual*/
-			if (dataVencimentoBoleto.before(dataAtualHoje)) {
-				System.out.println("Boleto vencido - URGENTE!");
-			} else {
-				System.out.println("Boleto não vencido!");
-			}
-		} catch (Exception e) {
+			calendar.add(Calendar.MONTH, 1);
+			System.out.println("Somando um mês :: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+			System.out.println();
+			
+			calendar.add(Calendar.YEAR, 1);
+			System.out.println("Somando um ano :: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+			
+		} catch (ParseException e) {
 			e.printStackTrace();
-			throw new Excecao(e.getMessage());
 		}
+		
 	}
 }
