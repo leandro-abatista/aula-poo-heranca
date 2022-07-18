c7b004449a2a560d6436784074e4cf7456d31d22
Leandro Amorim <leandroamorimpb@gmail.com>
2022-07-18 15:33:13 -0300

    Diferentes formas de atribuição de valores :white_check_mark:

---
+++ src/cursojava/main/ArrayVetor2.java
@@ -0,0 +1,15 @@
+package cursojava.main;
+
+import javax.swing.JOptionPane;
+
+public class ArrayVetor2 {
+
+	public static void main(String[] args) {
+		
+		
+		int[] inteiros = new int[10];
+		float[] floates = new float[10];
+		
+		
+	}
+}
